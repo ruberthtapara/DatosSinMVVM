227aef79bd1ec7d16290ab51c62f43b4117a2be6
ruberthtapara <ruberth.tapara@tecsup.edu.pe>
2026-05-02 09:25:12 -0500

    implementar interfaz de usuario ScreenUser e integración con Room

--- app/src/main/java/com/example/datossinmvvm/MainActivity.kt
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.datossinmvvm
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,24 +14,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            // Verifica que este nombre coincida con el de tu ui/theme/Theme.kt
             DatosSinMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Aplicamos el padding del Scaffold para que el contenido no quede bajo el notch
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        ScreenUser()
+                    }
                 }
             }
         }
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
